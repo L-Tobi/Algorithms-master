dcfb3543076e8db4d28b43f1491a04de1255b6f4
Tobi <liupenghui_best@163.com>
2018-03-23 00:11:38 +0800

    新增选择排序

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "insertion_sort.hpp"
+#include "sort.hpp"
 #include "linear_find.hpp"
 #include "math_algorithms.hpp"
 #include <iostream>
@@ -6,12 +6,18 @@
 
 int main(int argc, char** argv)
 {
-	
-	const int n = 10;
-	
-	std::bitset<n> a("1001010101");
-	std::bitset<n> b("1011011111");
-	std::cout << BinaryAdd(a, b) << std::endl;
+
+	std::vector<int> src{ 31, 41,59,26,41,58,20,100,2 };
+	ChooseSort<int>(src);
+	for (int i = 0; i < src.size(); i++)
+	{
+		printf("%d\n", src[i]);
+	}
+
+	//const int n = 10;	
+	//std::bitset<n> a("1001010101");
+	//std::bitset<n> b("1011011111");
+	//std::cout << BinaryAdd(a, b) << std::endl;
 
 /*
 	std::vector<int> src{ 31, 41,59,26,41,58 };
--- sort.hpp
+++ sort.hpp
@@ -1,5 +1,32 @@
 #include<vector>
 
+//Time Complex: N2
+template<class T>
+int ChooseSort(std::vector<T>& sort)
+{
+	if (sort.empty())
+	{
+		return 0;
+	}
+	for (int i = 0; i < sort.size() - 1; i++)
+	{
+		int _min = sort[i];
+		int _min_index = i;
+		for (int j = i + 1; j < sort.size(); j++)
+		{
+			if (_min > sort[j])
+			{
+				_min = sort[j];
+				_min_index = j;
+			}
+		}
+		sort[_min_index] = sort[i];
+		sort[i] = _min;
+	}
+	return 1;
+}
+
+//Time Complex: N2
 template<class T, bool sort_type_up = true>
 int InsertionSort(std::vector<T>& sort)
 {
