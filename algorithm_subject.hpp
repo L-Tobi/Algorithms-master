7de55aa8ccdec41567775761b41a56acadcf1068
Tobi <liupenghui_best@163.com>
2018-03-27 21:19:05 +0800

    最大子数组算法修正BUG版本

--- algorithm_subject.hpp
+++ algorithm_subject.hpp
@@ -25,46 +25,32 @@ void FindMaximumSubarray_Recursion(const std::vector<int> src, int& low, int& hi
 	int _mid = (low + high) >> 1;
 	int _left_res[3] = { low, _mid, INT_MIN };
 	int _right_res[3] = { _mid + 1, high, INT_MIN };
-	//int _right_high = high;
-	
-	//high = _mid;
-	//sum = INT_MIN;
 	FindMaximumSubarray_Recursion(src, _left_res[LOW_INDEX], _left_res[HIGH_INDEX], _left_res[RESULT_SUM]);
-	//_left_res[LOW_INDEX] = low;
-	//_left_res[HIGH_INDEX] = high;
-	//_left_res[RESULT_SUM] = sum;
-	/*low = _mid + 1;
-	high = _right_high;
-	sum = INT_MIN;*/
 	FindMaximumSubarray_Recursion(src, _right_res[LOW_INDEX], _right_res[HIGH_INDEX], _right_res[RESULT_SUM]);
-	//_right_res[LOW_INDEX] = low;
-	//_right_res[HIGH_INDEX] = high;
-	//_right_res[RESULT_SUM] = sum;
 
 	{
 		int _left_sum = INT_MIN;
 		int _low_index = -1;
 		int _right_sum = INT_MIN;
 		int _high_index = -1;
-
+		int _tmp = 0;
 		for (int i = _mid; i >= _left_res[LOW_INDEX]; i--)
 		{
-			static int _mid = 0;
-			_mid += src[i];
-			if (_mid > _left_sum)
+			_tmp += src[i];
+			if (_tmp > _left_sum)
 			{
-				_left_sum = _mid;
+				_left_sum = _tmp;
 				_low_index = i;
 			}
 		}
 
+		_tmp = 0;
 		for (int j = _mid + 1; j <= _right_res[HIGH_INDEX]; j++)
 		{
-			static int _mid = 0;
-			_mid += src[j];
-			if (_mid > _right_sum)
+			_tmp += src[j];
+			if (_tmp > _right_sum)
 			{
-				_right_sum = _mid;
+				_right_sum = _tmp;
 				_high_index = j;
 			}
 		}
--- main.cpp
+++ main.cpp
@@ -11,9 +11,8 @@ int main(int argc, char** argv)
 {
 	srand((unsigned)time(0));
 
-//	std::vector<int> src;
-	std::vector<int> src{ 2, 3, -4, 5, -2, -3, 8, -1, -3, 1, 0, -5, 1, 4, -2, 6, 2, 1, -3, -5, -2, 0, -1, 0, 3, 2, 3, 4, 5, -2, -3 };
-	
+//	std::vector<int> src{ 2, 3, -4, 5, -2, -3, 8, -1, -3, 1, 0, -5, 1, 4, -2, 6, 2, 1, -3, -5, -2, 0, -1, 0, 3, 2, 3, 4, 5, -2, -3 };
+	std::vector<int> src{ 2, 3, -4, 5, -2, -3, -8, 90, -3, 1, 0, -5, 1, 4, -2, 6, 2, 1, -3, -5, -2, 0, -1, 0, 3, 2, 3, -4, 5, -2, -3 };
 	//int a[10] = { 1,2,3,4,5,6,7,8,9,10 };
 	//std::array<int, 100> _search;
 	//for (int i = 0; i < 100; i++)
@@ -29,7 +28,7 @@ int main(int argc, char** argv)
 	//	printf("%d\n", _search[i]);
 //	printf("BS : %d\n", BinarySearch<int, 100>(_search, 200, 0, 100));
 	int low = 0;
-	int high = src.size();
+	int high = src.size()-1;
 	int sum = 0;
 	FindMaximumSubarray_Recursion(src, low, high, sum);
 	printf("low : %d  high : %d  sum : %d\n", low, high, sum);
