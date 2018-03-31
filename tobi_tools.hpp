aad567b7482f430b5a2283c3438d6c2e4097c964
Tobi <liupenghui_best@163.com>
2018-03-31 23:22:02 +0800

    新增堆排序算法

--- tobi_tools.hpp
+++ tobi_tools.hpp
@@ -25,8 +25,8 @@ namespace tobi {
 		std::random_shuffle(array.begin(), array.end());
 	}
 
-	template<class T = std::vector<int>, bool feed = true>
-	void PrintVector(const T vector)
+	template<class T, bool feed = true>
+	void PrintVector(const std::vector<T> vector)
 	{
 		if (feed)
 		{
